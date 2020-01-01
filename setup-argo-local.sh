f523bd2600e6991082878c680fca1d1ed7408085
Nikolai <nikolai.topping@gmail.com>
2020-01-01 12:46:08 +0100

    auto sync

--- setup-argo-local.sh
+++ setup-argo-local.sh
@@ -1,6 +1,8 @@
-./setup-kubeseal-local
+#!/bin/bash
+. create-sealed-github-secret.sh
 kubectl create namespace argocd
 kustomize build src/overlays/local | kubectl apply -f -
+kubectl get pods -n argocd -l app.kubernetes.io/name=argocd-server -o name | cut -d'/' -f 2
 ### 
 # Access with port-forward using command:
 # kubectl port-forward svc/argocd-server -n argocd 8001:443
